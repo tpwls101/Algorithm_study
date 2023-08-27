b42c78d139744becb4652f40975952c5753dad84
유세진 <97137124+tpwls101@users.noreply.github.com>
2023-08-27 16:51:36 +0900

    [Gold IV] Title: 알파벳, Time: 1008 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
  * <BJ_1987_알파벳>
  * 배열의 x,y좌표와 count를 매개변수로 전달하여 dfs 호출
  * 이 때, 처음 칸도 count에 해당하므로 1 전달
- * alphabet[] 배열에 내가 이동 중에 알파벳을 사용했으면 true로 변경
- * 그리고 4방을 돌면서 범위를 벗어나면 다음 방향 탐색, 아니면 이미 사용중인 알파벳인지 확인, 아니라면 다음 재귀 호출
- * 방문을 마쳤을 때 다시 돌아와서 방문확인을 false로 변경
- * 매번 count가 최대값인지 확인해서 갱신
+ * alphabets[] 배열에 내가 이동 중에 사용한 현재 알파벳을  true로 변경
+ * 그리고 4방을 돌면서 배열의 범위를 벗어나지 않으면 먼저 max값 갱신
+ * 그리고 사용한 알파벳이 아니라면 다음 재귀 호출
+ * 현재 알파벳에서 4방 탐색 후 사용할 수 없으면 다시 방문 처리 취소
  * 
  * @author 유세진
  *
@@ -20,64 +20,58 @@ public class Main {
 	
 	static int R; // 세로 R칸
 	static int C; // 가로 C칸
-	static char[][] arr; // 알파벳을 저장할 배열
-	static boolean[] alphabet; // 이동한 경로를 따라 알파벳이 사용되었는지를 저장할 배열
-	static int max = Integer.MIN_VALUE;
+	static char[][] input; // 입력받은 알파벳을 저장할 배열
+	static boolean[] alphabets = new boolean[26]; // 알파벳을 사용했는지 확인할 배열
+	static int max = Integer.MIN_VALUE; // 말이 지날 수 있는 최대의 칸 수를 갱신하기 위해 사용
 	
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
+	// 우하좌상
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
 	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		R = Integer.parseInt(st.nextToken()); // 세로 R칸
-		C = Integer.parseInt(st.nextToken()); // 가로 C칸
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 		
-		arr = new char[R][C]; // 알파벳을 저장할 배열
-		alphabet = new boolean[26];
+		input = new char[R][C]; // 알파벳을 입력받아 저장할 배열
 		
 		// 알파벳 입력받기
 		for(int i=0; i<R; i++) {
-			String str = br.readLine();
+			char[] cArr = br.readLine().toCharArray();
 			for(int j=0; j<C; j++) {
-				arr[i][j] = str.charAt(j);
+				input[i][j] = cArr[j];
 			}
 		}
 		
-		dfs(0, 0, 1);
+		dfs(0, 0, 1); // 시작 좌표에서 이미 카운트 1
 		
 		System.out.println(max);
-		
 	}
 	
+	// x : 현재 x좌표
+	// y : 현재 y좌표
+	// count : 말이 지난 칸 수
 	private static void dfs(int x, int y, int count) {
-
-		alphabet[arr[x][y] - 65] = true; // 사용한 알파벳 체크
+		char alpha = input[x][y]; // 현재 좌표의 알파벳 확인
+		alphabets[alpha - 65] = true; // 현재 알파벳 사용중임
 		
 		for(int i=0; i<4; i++) {
 			int temp_x = x + dx[i];
 			int temp_y = y + dy[i];
 			
-			// 배열의 범위를 벗어나면 다음 탐색
-			if(temp_x < 0 || temp_x >= R || temp_y < 0 || temp_y >= C) {
-				continue;
+			// 배열의 범위를 벗어나지 않으면
+			if(temp_x >= 0 && temp_x < R && temp_y >= 0 && temp_y < C) {
+				max = Math.max(max, count); // 말이 지날 수 있는 최대 칸 수 갱신
+				// 사용한 알파벳이 아니면 탐색
+				if(!alphabets[input[temp_x][temp_y] - 65]) {
+					dfs(temp_x, temp_y, count+1);
+				}
 			}
-				
-			// 이미 사용한 알파벳이면 리턴
-			if( alphabet[ arr[temp_x][temp_y] - 65 ] ) { // 이미 사용한 알파벳
-				continue;
-			}
-			
-			// 사용한 알파벳이 아니라면 다음 탐색 (재귀 호출)
-			dfs(temp_x, temp_y, count+1);
-			
-			alphabet[ arr[temp_x][temp_y] - 65] = false; // 다음에 다시 방문할 수도 있으니 false 처리
-			
 		}
 		
-		max = Math.max(max, count);
+		alphabets[alpha - 65] = false; // 현재 알파벳에서 4방 탐색 후 사용할 수 없으면 다시 방문 처리 취소
 	}
 
 }
