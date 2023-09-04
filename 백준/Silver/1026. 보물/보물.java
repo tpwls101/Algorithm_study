f72acf604f50c29ad73ad9b645574777423646e5
유세진 <97137124+tpwls101@users.noreply.github.com>
2023-09-04 21:52:36 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[] A = new int[N];
+		Integer[] B = new Integer[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B, Collections.reverseOrder());
+		
+		int sum = 0;
+		for(int i=0; i<N; i++) {
+			sum += A[i] * B[i];
+		}
+		
+		System.out.println(sum);
+	}
+}
