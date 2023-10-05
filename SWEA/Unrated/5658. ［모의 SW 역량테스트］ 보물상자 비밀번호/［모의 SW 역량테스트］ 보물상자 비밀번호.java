d3265d59dc3d30d13d0b0662817976c532355251
유세진 <97137124+tpwls101@users.noreply.github.com>
2023-10-05 22:11:50 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 119 ms, Memory: 22,124 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -17,16 +17,9 @@ import java.util.TreeSet;
  * 0회전, 1회전, 2회전까지 가능! -> 3회전하면 0회전과 동일!
  * 주의 : 서로 다른 회전 횟수에서 동일한 수가 중복으로 생성될 수 있음!! -> 크기에 따라 순서를 셀 때 같은 수를 중복으로 세지 말 것!
  * 
- * 1. 한 변이 세자리수(N/4) 일 때 2회전(N/4 - 1)까지 가능
- * 2. numbers 배열을 세자리수(N/4)만큼씩 끊어서 회전별로 생성 가능한 수를 만든다.
- *         0회전 : 1B3, B3B, 81F, 75E
- *         1회전 : 
- *         2회전 : 
- *         => 회전할 때마다 shift(>>) 연산자 써보기?
- * 3. 생성한 수를 Set 자료구조에 넣어서 중복 없애기
- * 4. 다시 ArrayList로 넣어서 오름차순 정렬
- * 
- * 너무 노가다............! (X) => 자료구조를 활용하자!
+ * !! Queue와 TreeSet 사용 !!
+ * Queue : 비밀번호를 한 칸씩 밀기 위해 사용
+ * TreeSet : 중복 제거와 정렬을 해결하기 위해 사용
  * 
  * @author 유세진
  *
@@ -36,8 +29,6 @@ public class Solution {
     
     static int N; // 숫자의 개수
     static int K; // 크기 순서
-    //static String[] numbers; // 입력받은 16진수 숫자들을 저장할 배열
-    
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -53,7 +44,7 @@ public class Solution {
             
             
             Queue<String> queue = new ArrayDeque<>();
-            TreeSet<String> set = new TreeSet<>(Collections.reverseOrder());
+            TreeSet<String> set = new TreeSet<>(Collections.reverseOrder()); // set에 넣으면 자동 내림차순 정렬
             
             // 처음 한 줄 입력받은 값 큐에 넣기
             String[] s = br.readLine().split("");
@@ -63,7 +54,6 @@ public class Solution {
             
             // 보물상자 뚜껑 size-1 만큼 회전
             for(int i=0; i<size; i++) {
-                
                 // 비밀번호 TreeSet에 넣기
                 for(int a=0; a<4; a++) { // 비밀번호는 4개니까 4번 반복
                 	String pw = "";
@@ -78,17 +68,12 @@ public class Solution {
                 // 한칸씩 밀어주기
                 String one = queue.poll(); // 맨 앞에꺼 빼서
                 queue.offer(one); // 맨 뒤에 넣어주기
-                
             }
             
+            // set은 인덱스 번호가 없으니까 배열로 바꿔서 출력해주기
+            String[] arr = set.toArray(new String[0]);
+            System.out.println("#" + tc + " " + Integer.parseInt(arr[K-1], 16)); // 16진수 문자열 -> 10진수 정수로 바꿔서 출력
             
-            	String[] arr = set.toArray(new String[0]);
-            
-            	
-            	
-            	
-            	
-            	System.out.println("#" + tc + " " + Integer.parseInt(arr[K-1], 16));
 //            Iterator iter = set.iterator();
 //            int cnt = 1;
 //            while(iter.hasNext()) {
@@ -102,9 +87,9 @@ public class Solution {
 //            	}
 //            }
             
-            // 다음 테케 실행 위해서 큐랑 셋 비우기!!!!!!
-            queue.clear();
-            set.clear();
+            // 다음 테케 실행 위해서 큐랑 셋 비우기!!!!!! -> 위에서 선언하면서 초기화 해주므로 안해도 무방
+//            queue.clear();
+//            set.clear();
         }
         
     }
