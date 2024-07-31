e3b84c4874dc47055af6120410ccfc5a83c38cff
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-07-31 17:31:19 +0900

    Update and rename 차집합.java to 차집합_방법1.java

--- 백준/Silver/1822. 차집합/차집합_방법1.java
+++ 백준/Silver/1822. 차집합/차집합_방법1.java
@@ -6,7 +6,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * <방법1>
+ * 해시를 사용한 집합과 맵
+ * Hash는 시간복잡도가 O(1)이므로 속도는 훨씬 빠르다.
+ * 
+ * <방법2>
+ * 트리를 사용한 집합과 맵
+ * 정렬을 유지하기 위해 Hash가 아닌 Tree 자료구조 사용
+ * value 값만 확인하면 되므로 TreeSet 사용
+ * TreeSet을 사용하면 따로 정렬할 필요는 없지만 Tree의 시간복잡도는 O(logN)이므로 Hash에 비해 속도가 느리다.
+ * 
+ * @author YooSejin
+ *
+ */
+
+public class BJ_1822_차집합_방법1 {
 	
 	static int N; // 집합 A의 원소의 개수
 	static int M; // 집합 B의 원소의 개수
