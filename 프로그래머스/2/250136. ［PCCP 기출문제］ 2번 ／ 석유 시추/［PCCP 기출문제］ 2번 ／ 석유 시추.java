3b949f3f9da923d18f0368be284520045667a7fb
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-08-05 14:29:02 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 90.75 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+/**
+bfs 돌 동안 y좌표를 set에 저장
+set에 저장된 열에 해당하는 oil[] 배열에 석유 크기 저장
+set은 초기화
+
+방문배열은 초기화x
+oil[] 배열에 석유 크기 누적해서 저장
+*/
+
+class Solution {
+    
+    static int[] dx = { 0, 1, 0, -1 };
+    static int[] dy = { 1, 0, -1, 0 };
+    
+    static int row; // 행
+    static int col; // 열
+    static boolean visited[][]; // 방문배열
+    static int size = 0; // 석유의 크기
+    static int[] oil; // 누적 석유 크기를 저장할 배열
+    
+    static class Node {
+        int x;
+        int y;
+        
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public int solution(int[][] land) {
+        
+        row = land.length;
+        col = land[0].length;
+        
+        visited = new boolean[row][col];
+        oil = new int[col];
+            
+        for(int i=0; i<col; i++) {
+            // 매 열마다 방문배열 초기화 필요
+            // visited = new boolean[row][col];
+            
+            int sum = 0; // 한 열을 뚫을 때의 석유의 합
+            for(int j=0; j<row; j++) {                
+                // 석유가 있고 아직 방문하지 않았다면 크기 탐색
+                if(land[j][i] == 1 && !visited[j][i]) {
+                    // sum += bfs(new Node(j, i), land);
+                    bfs(new Node(j, i), land);
+                }
+            }
+            // answer = Math.max(answer, sum);
+        }
+        
+        Arrays.sort(oil);
+        return oil[oil.length-1];
+    }
+    
+    static void bfs(Node node, int[][] land) {
+        int count = 0; // 한번 탐색할 때 석유의 크기
+        
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+        visited[node.x][node.y] = true;
+        count++;
+        
+        Set<Integer> set = new HashSet<>();
+        set.add(node.y);
+        
+        while(!queue.isEmpty()) {
+            Node current = queue.poll();
+            
+            for(int i=0; i<4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+                
+                if(nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                    if(land[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.add(new Node(nx, ny));
+                        visited[nx][ny] = true;
+                        count++;
+                        set.add(ny);
+                    }
+                }
+            }
+        }
+        
+        // set에 저장되어 있는 열에 석유 크기 저장하기
+        for(int col : set) {
+            oil[col] += count;
+        }
+        // for(int i=0; i<oil.length; i++) {
+        //     System.out.print(oil[i] + " ");
+        // }
+        // System.out.println();
+        // return count;
+    }
+}
