3d032ccc107b4fdf34c43b1c08c7f4f92b218b65
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-08-27 23:33:42 +0900

    Update K번째 수.java

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
