d90a782ac7f7e5766e8bce6c53c679168b6c3947
tpwls101 <97137124+tpwls101@users.noreply.github.com>
2024-09-12 13:34:59 +0900

    Update and rename 더 맵게.java to 더 맵게_방법2.java

--- 프로그래머스/2/42626. 더 맵게/더 맵게_방법2.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게_방법2.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
+/**
+효율성 통과가 중요!!
+scoville 배열의 길이와 K의 범위가 크기 때문에 list를 사용하면 시간초과 난다.
+while문 안에서 list를 정렬하면 
+    Collections.sort() : O(NlogN)
+    정렬을 계속해서 반복하므로 결국 O(N^2logN)의 시간복잡도를 가진다.
+
+따라서 Heap 자료구조를 활용해야 한다.
+힙은 삽입/삭제에 O(logN)의 시간복잡도를 가진다.
+while문을 아무리 돌려도 결국 O(NlogN)의 시간복잡도를 가지므로 괜찮다.
+*/
+
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
