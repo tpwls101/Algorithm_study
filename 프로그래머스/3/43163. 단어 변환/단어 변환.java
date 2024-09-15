602b679bbcee9a03205501e3b23957ee2a2fecb7
tpwls101 <97137124+tpwls101@users.noreply.github.com>
2024-09-15 19:28:30 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,58 @@
+/**
+처음 실수!
+begin을 전역변수로 사용해서 단어 변환이 가능하면 begin = words[i]; 로 바꿔줬는데
+begin을 바꿔버리면 이후에 target과 비교가 불가능하다.
+
+*/
+
+class Solution {
+    
+    static String begin;
+    static String target;
+    static String[] words;
+    static boolean[] visited;
+    // static int count = Integer.MAX_VALUE; // 변환 횟수
+    static int count = 0;
+    
+    public int solution(String begin, String target, String[] words) {
+        this.begin = begin;
+        this.target = target;
+        this.words = words;
+        
+        visited = new boolean[words.length];
+        
+        // if(!Arrays.asList(words).contains(target)) {
+        //     return 0;
+        // }
+        
+        dfs(begin, 0); // count 전달. 아직 0번 바뀜.
+        
+        return count;
+    }
+    
+    static public void dfs(String currentWord, int cnt) {
+        if(currentWord.equals(target)) {
+            //count = Math.min(cnt, count);
+            count = cnt;
+            return;
+        }
+        
+        for(int i=0; i<words.length; i++) {
+            if(visited[i]) continue;
+            
+            // 단어 비교 : 다른 알파벳이 하나여야 변환 가능
+            int differentCnt = 0; // 다른 알파벳의 수
+            for(int j=0; j<currentWord.length(); j++) {
+                if(currentWord.charAt(j) != words[i].charAt(j)) {
+                    differentCnt++;
+                }
+            }
+            
+            if(differentCnt == 1) {
+                visited[i] = true;
+                dfs(words[i], cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
