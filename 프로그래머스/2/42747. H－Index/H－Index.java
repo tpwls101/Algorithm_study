266e96a2da07f0af0ba1a2ab9c48f9ba327f273e
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-09-29 17:25:48 +0900

    Update H－Index.java

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,5 +1,10 @@
 import java.util.*;
 
+/**
+말이 너무 헷갈린다
+문제 별로임 ㅜㅜ
+*/
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
@@ -9,6 +14,7 @@ class Solution {
         for(int i=0; i<citations.length; i++) {
             int h = citations.length - i; // citations[i]번 이상 인용된 횟수
             
+            // ??? ㅜㅜ
             if(citations[i] >= h) {
                 answer = h;
                 break;
