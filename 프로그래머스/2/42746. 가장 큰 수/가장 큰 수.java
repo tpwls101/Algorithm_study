a46eb32057220e0dad0dd05adb9a73dfd83844b3
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-09-29 16:09:15 +0900

    [level 2] Title: 가장 큰 수, Time: 3136.91 ms, Memory: 400 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+numbers의 길이는 10만까지고 원소는 1000까지 가능하므로
+최악의 경우 네자리수가 10만개 있게 된다.
+애초에 숫자로 환산할 수 없는 길이!!!! -> int나 long 타입을 사용할 수 없다!!
+
+따라서 numbers를 문자열로 바꿔 내림차순 정렬한다.
+단, 3과 30을 비교했을 때 [30, 3]이 아니라 [3, 30]이 되어야 함을 주의!!
+따라서 두개씩 묶어서 비교한다.
+*/
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] str = new String[numbers.length];
+        
+        for(int i=0; i<str.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
+        }
+        
+        // 두개씩 묶어서 내림차순 정렬
+        Arrays.sort(str, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+        
+        String answer = "";
+        if(str[0].equals("0")) {
+            return "0";
+        }
+        
+        for(String s : str) {
+            answer += s;
+        }
+        
+        return answer;
+    }
+}
