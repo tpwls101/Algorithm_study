939cc2c88c706f9c4c930ecfc0ae4990953a19a3
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-11-04 23:31:37 +0900

    Update 한 줄로 서기.java

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -26,31 +26,31 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        	StringTokenizer st = null;
         
-        N = Integer.parseInt(br.readLine());
+        	N = Integer.parseInt(br.readLine());
 		
-        answer = new int[N];
+        	answer = new int[N];
         
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-        	int num = Integer.parseInt(st.nextToken()); // 자기보다 키가 큰 사람의 수
-        	
-        	int cnt = 0;
-        	for(int j=0; j<N; j++) {
-        		if(cnt == num) {
-        			if(answer[j] == 0) {
-        				answer[j] = i+1;
-        				break;
-        			}
-        		}
-        		if(answer[j] == 0) cnt++;
-        	}
-        }
-		
-        for(int i=0; i<N; i++) {
-        	System.out.print(answer[i] + " ");
-        }
+	        st = new StringTokenizer(br.readLine());
+	        for(int i=0; i<N; i++) {
+	        	int num = Integer.parseInt(st.nextToken()); // 자기보다 키가 큰 사람의 수
+	        	
+	        	int cnt = 0;
+	        	for(int j=0; j<N; j++) {
+	        		if(cnt == num) {
+	        			if(answer[j] == 0) {
+	        				answer[j] = i+1;
+	        				break;
+	        			}
+	        		}
+	        		if(answer[j] == 0) cnt++;
+	        	}
+	        }
+			
+	        for(int i=0; i<N; i++) {
+	        	System.out.print(answer[i] + " ");
+	        }
 	}
 
 }
