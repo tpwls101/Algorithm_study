8ae7c29270ed9f96d12ba8aa4434a9212055199c
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-11-13 18:18:30 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 567.79 ms, Memory: 546 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[][] points;
+    static int[][] routes;
+    static Queue<Node>[] record; // 로봇의 최단경로를 큐로 저장한 배열
+    static int N; // 로봇의 개수
+    static int answer = 0; // 위험한 상황 횟수
+    
+    static class Node {
+        int x;
+        int y;
+        
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public int solution(int[][] points, int[][] routes) {
+        this.points = points;
+        this.routes = routes;
+        
+        N = routes.length;
+        record = new LinkedList[N];
+        
+        // record 배열 초기화
+        for(int i=0; i<N; i++) {
+            record[i] = new LinkedList<>();
+        }
+        
+        // 로봇의 최단거리 이동 경로 저장
+        recording();
+        
+        // 충돌 위험 세기
+        countCollision();
+        
+        return answer;
+    }
+    
+    // 로봇의 최단거리 이동 경로를 저장하는 함수
+    static void recording() {
+        for(int i=0; i<N; i++) {
+            // 로봇의 시작 위치 좌표
+            int robotNum = routes[i][0];
+            int x = points[robotNum-1][0];
+            int y = points[robotNum-1][1];
+            
+            // 로봇의 시작 위치를 큐에 추가
+            record[i].add(new Node(x, y));
+            
+            // 로봇의 경로에 따라 다음 포인트로 이동
+            for(int j=1; j<routes[i].length; j++) {
+                int nextRobotNum = routes[i][j];
+                int nx = points[nextRobotNum-1][0];
+                int ny = points[nextRobotNum-1][1];
+                
+                // 위아래로 먼저 이동하고 좌우로 이동한다
+                while(x != nx) { // 위아래(행)
+                    if(x < nx) x++;
+                    else x--;
+                    record[i].add(new Node(x, y)); // 이동한 좌표를 큐에 기록
+                }
+                
+                while(y != ny) { // 좌우(열)
+                    if(y < ny) y++;
+                    else y--;
+                    record[i].add(new Node(x, y));
+                }
+            }
+        }
+    }
+    
+    // 충돌 위험을 세는 함수
+    static void countCollision() {
+        int count = 0; // 경로가 끝난 로봇의 수
+        
+        // 모든 로봇의 경로가 끝날 때까지 반복
+        while(count < N) {
+            int[][] map = new int[101][101]; // 같은 시간에 좌표에 몇 개의 로봇이 지나는지 저장
+            count = 0; // 초기화
+            
+            // 같은 시간일 때 각 로봇의 위치를 맵에 기록
+            for(int i=0; i<N; i++) {
+                if(record[i].isEmpty()) {
+                    count++; // 경로가 끝난 로봇의 수 증가
+                    continue;
+                }
+                
+                Node current = record[i].poll();
+                map[current.x][current.y]++;
+            }
+            
+            // 맵에서 두 개 이상의 로봇이 지나갔으면 카운트
+            for(int i=0; i<101; i++) {
+                for(int j=0; j<101; j++) {
+                    if(map[i][j] > 1) answer++;
+                }
+            }
+        }
+    }
+}
