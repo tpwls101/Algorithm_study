af0f14d0a2321fde59abc4c9ad442691ef50ac45
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-11-13 16:17:01 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 20.97 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,34 +1,28 @@
 class Solution {
+    
+    static int videoLen;
+    static int position;
+    static int opStart;
+    static int opEnd;
+    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String answer = "";
-        
-        int position = toSec(pos); // 현재 위치
-        int opStart = toSec(op_start);
-        int opEnd = toSec(op_end);
-        int videoLen = toSec(video_len);
+        videoLen = toSec(video_len);
+        position = toSec(pos); // 현재 위치
+        opStart = toSec(op_start);
+        opEnd = toSec(op_end);
         
         for(int i=0; i<commands.length; i++) {
-            if(position >= opStart && position <= opEnd) {
-                position = opEnd;
-            }
+            openingCheck();
             
             if(commands[i].equals("prev")) {
-                if(position - 10 < 0) {
-                    position = 0;
-                } else {
-                    position -= 10;
-                }
+                if(position - 10 < 0) position = 0;
+                else position -= 10;
             } else if(commands[i].equals("next")) {
-                if(position + 10 > videoLen) {
-                    position = videoLen;
-                } else {
-                    position += 10;
-                }
+                if(position + 10 > videoLen) position = videoLen;
+                else position += 10;
             }
             
-            if(position >= opStart && position <= opEnd) {
-                position = opEnd;
-            }
+            openingCheck();
         }
         
         int m = position / 60;
@@ -42,5 +36,11 @@ class Solution {
         int m = Integer.parseInt(sArr[0]);
         int s = Integer.parseInt(sArr[1]);
         return m * 60 + s;
+    }
+    
+    static void openingCheck() {
+        if(position >= opStart && position <= opEnd) {
+            position = opEnd;
+        }
     }
 }
