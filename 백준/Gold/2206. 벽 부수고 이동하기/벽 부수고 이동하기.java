2b0d04706384c18656393e349f5ee150a073d02b
유세진 <97137124+tpwls101@users.noreply.github.com>
2024-11-20 17:19:20 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1216 ms, Memory: 184852 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -12,6 +12,7 @@ import java.util.StringTokenizer;
  * 왜냐하면 벽을 부수지 않았을 때 거리가 더 짧은데, 이미 벽을 부숴서 방문해버리면 벽을 부수지 않은 길에서 방문할 수 없기 때문이다.
  * 
  * !! 3차원 방분 배열 처리에 주의할 것 !!
+ * (N,M)에 도착하면 최단 거리 찾고 종료해야 되는데 return 안써서 시간 버림 ㅜㅜ
  * 
  * @author YooSejin
  *
@@ -74,51 +75,51 @@ public class Main {
 			
 			if(current.x == N-1 && current.y == M-1) {
 				answer = current.distance;
-				return;
+				return; // ;;; 정답 찾았으면 종료해야 되는데 return 안써서 시간 버림 ㅜㅜ
 			}
 			
 			for(int i=0; i<4; i++) {
 				int nx = current.x + dx[i];
 				int ny = current.y + dy[i];
 				
-//				if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
-//					if(arr[nx][ny] == 1) { // 다음 칸이 벽일 때
-//						if(current.brokenWall == 0 && !visited[nx][ny][1]) { // 현재 부순 벽이 아직 없다면
-//							queue.add(new Node(nx, ny, current.brokenWall+1, current.distance+1));
-//							visited[nx][ny][1] = true;
-//						}
-//					} else { // 다음 칸이 빈 칸일 때
-//						// 현재 부순 벽이 있냐 없냐로 구분해서 방문 배열 처리
-//						if(current.brokenWall == 0 && !visited[nx][ny][0]) { // 아직 부순 벽이 없으면 -> visited[][][0] 확인
-//							queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
-//							visited[nx][ny][0] = true;
-//						} else if(current.brokenWall == 1 && !visited[nx][ny][1]) { // 부순 벽이 있으면 -> visited[][][1] 확인
-//							queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
-//							visited[nx][ny][1] = true;
-//						}
-//					}
-//				}
-				
 				if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
-					if(current.brokenWall == 1) { // 벽을 부순적이 있다면
-						if(arr[nx][ny] == 0 && !visited[nx][ny][1]) { // 빈 칸만 지나갈 수 있음
+					if(arr[nx][ny] == 1) { // 다음 칸이 벽일 때
+						if(current.brokenWall == 0 && !visited[nx][ny][1]) { // 현재 부순 벽이 아직 없다면
+							queue.add(new Node(nx, ny, current.brokenWall+1, current.distance+1));
+							visited[nx][ny][1] = true;
+						}
+					} else { // 다음 칸이 빈 칸일 때
+						// 현재 부순 벽이 있냐 없냐로 구분해서 방문 배열 처리
+						if(current.brokenWall == 0 && !visited[nx][ny][0]) { // 아직 부순 벽이 없으면 -> visited[][][0] 확인
+							queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
+							visited[nx][ny][0] = true;
+						} else if(current.brokenWall == 1 && !visited[nx][ny][1]) { // 부순 벽이 있으면 -> visited[][][1] 확인
 							queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
 							visited[nx][ny][1] = true;
 						}
-					} else if(current.brokenWall == 0) { // 벽을 부순적이 없다면
-						if(arr[nx][ny] == 1) { // 다음 벽이 벽이라면
-							if(!visited[nx][ny][1]) {
-								queue.add(new Node(nx, ny, current.brokenWall+1, current.distance+1));
-								visited[nx][ny][1] = true;
-							}
-						} else { // 다음 벽이 빈 칸이라면
-							if(!visited[nx][ny][0]) {
-								queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
-								visited[nx][ny][0] = true;
-							}
-						}
 					}
 				}
+				
+//				if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
+//					if(current.brokenWall == 1) { // 벽을 부순적이 있다면
+//						if(arr[nx][ny] == 0 && !visited[nx][ny][1]) { // 빈 칸만 지나갈 수 있음
+//							queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
+//							visited[nx][ny][1] = true;
+//						}
+//					} else if(current.brokenWall == 0) { // 벽을 부순적이 없다면
+//						if(arr[nx][ny] == 1) { // 다음 벽이 벽이라면
+//							if(!visited[nx][ny][1]) {
+//								queue.add(new Node(nx, ny, current.brokenWall+1, current.distance+1));
+//								visited[nx][ny][1] = true;
+//							}
+//						} else { // 다음 벽이 빈 칸이라면
+//							if(!visited[nx][ny][0]) {
+//								queue.add(new Node(nx, ny, current.brokenWall, current.distance+1));
+//								visited[nx][ny][0] = true;
+//							}
+//						}
+//					}
+//				}
 			}
 		}
 	}
