d623f819b4e3f4effbd825fdfffb4adec11f419c
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-02-06 13:19:31 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -2,6 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * <BJ_9461_파도반수열>
+ * 점화식은 찾아 맞았지만 답의 범위 고려를 못했다.
+ * 근데 N=100일 때 int형의 범위를 넘는다는건 어떻게 아는걸까...?
+ * 직접 찍어봐서??
+ * 그렇다. 직접 찍어보는게 제일 빠르다..ㅎ
+ * 가장 최악의 범위를 입력해 직접 출력값을 확인해보자. 오버플로우가 발생하면 long 타입을 써줄 것!!
+ * 
+ * @author YooSejin
+ *
+ */
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
