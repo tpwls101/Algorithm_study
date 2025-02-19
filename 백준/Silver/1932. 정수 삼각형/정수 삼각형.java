708ba59af059bbef2d827167fcf016cd1555467f
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-02-19 15:10:29 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 26884 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -3,6 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * <BJ_1932_정수삼각형>
+ * dfs로 풀면 될 것 같지만 범위가 500이므로 시간초과 날 것 같다.
+ * 그리고 그리디는 예외가 생기고, 부분 구조를 만족하기에 DP로 풀어야겠다고 생각.
+ * 각 dp[i][j]에는 칸까지 올 수 있는 경로의 최댓값을 저장함.
+ * (N+1)x(N+1) 크기의 배열로 생각해 볼 때, dp[i][j]칸은 dp[i-1][j-1]과 dp[i-1][j]에서 올 수 있음.
+ * 둘 중 최댓값으로 갱신해 각 dp에 저장하면 됨.
+ * 생각보다 빠르게 풀 수 있었음. 드디어!!
+ * 
+ * @author YooSejin
+ *
+ */
+
 public class Main {
 	
 	static int N; // 삼각형의 크기
