33221b6d53e1350f7dd620cc4c07292bb8d24340
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-04-29 16:13:20 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 35120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * <BJ_15649_N과M_1>
+ * 순열
+ * 
+ * @author YooSejin
+ *
+ */
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static boolean[] visited;
+	static int[] selected;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        visited = new boolean[N+1];
+        selected = new int[M];
+		
+        perm(0);
+        
+        System.out.println(sb);
+	}
+	
+	static void perm(int cnt) {
+		if(cnt == M) {
+			for(int num : selected) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(visited[i]) continue;
+			visited[i] = true;
+			selected[cnt] = i;
+			perm(cnt+1);
+			visited[i] = false;
+		}
+	}
+
+}
