6a868bf9284aa6910bf89927fa2dcde0c12b97ba
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-07-21 23:33:07 +0900

    [Gold IV] Title: 두 동전, Time: 160 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.java
+++ 백준/Gold/16197. 두 동전/두 동전.java
@@ -2,12 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
  * <BJ_16197_두동전>
+ * 최소값을 구하는 BFS 문제
+ * 
  * 두 동전이 '동시에' 움직이기 때문에 각 동전 객체에 이동 횟수를 저장할 필요 없고 count로 한번에 처리해도 된다.
+ * 각 동전의 위치를 한번에 큐에 담아 뽑은 다음, 각각 다음 위치를 탐색해 조건에 맞게 처리하면 된다.
+ * 즉, 동전1의 위치와 동전2를 동시에 한꺼번에 처리.
  * 
  * !! 주의할 점 !!
  * while문에서 for문을 돌릴 때 큐의 사이즈만큼 for문을 돌리고나서 count를 증가해야 한다.
@@ -15,6 +21,12 @@ import java.util.StringTokenizer;
  * 만약 큐 사이즈로 for문을 돌리지 않으면 버튼을 두번 눌러 가능한 좌표에서 처음으로 새롭게 좌표를 만들고 count가 증가해 3이 된후
  * 다른 좌표에서 또 새롭게 좌표를 만들어 count가 4가 되는 일이 발생한다.
  * 하지만 새로운 두 좌표는 모두 두번 누른 후 추가로 누른 것이므로 count가 3이 되어야 한다.
+ * 
+ * !! 방문처리 !!
+ * 방문처리를 안해도 통과되지만 큐에 중복값이 많이 들어가기 때문에 해주면 훨씬 효율적이다.
+ * 다만 방문배열을 사용하기에는 두 동전이 동시에 움직이고, 두개의 배열을 사용한다 해도 이미 방문했던 자리를 벽으로 가로막혀 또 방문할 수 있으므로 적합하지 않다.
+ * 따라서 중복값을 가지지 않고 조회가 쉬운 HashSet을 사용한다.
+ * 두 동전의 좌표를 String으로 만들어 합친 후 저장하고, 이후 해당 좌표를 다시 방문하면 큐에 값을 넣지 않는다.
  * 
  * @author YooSejin
  *
@@ -36,6 +48,11 @@ public class Main {
 		Node(int x, int y) {
 			this.x = x;
 			this.y = y;
+		}
+		
+		@Override
+		public String toString() {
+			return "" + x + y; // 동전의 좌표를 String으로 HashSet에 저장하기 위해 사용
 		}
 	}
 	
@@ -72,9 +89,12 @@ public class Main {
 	
 	static int bfs(Node coin1, Node coin2) {
 		Queue<Node[]> queue = new ArrayDeque<>();
-		queue.add(new Node[] {coin1, coin2});
+		Set<String> visited = new HashSet<>();
 		
-		int count = 1; // 이동 횟수
+		queue.add(new Node[] {coin1, coin2});
+		visited.add(coin1.toString() + coin2.toString());
+		
+		int count = 1; // 버튼을 누른 횟수
 		
 		while(!queue.isEmpty()) { // 동전 하나가 보드 밖으로 나갈 때까지 반복
 			if(count > 10) return -1; // 버튼을 10번보다 많이 눌러야 하면 -1 출력
@@ -93,7 +113,6 @@ public class Main {
 					
 					// 둘 중 하나만 보드 밖으로 나가야 성공
 					if((isOut(nx1, ny1) && !isOut(nx2, ny2)) || (!isOut(nx1, ny1) && isOut(nx2, ny2))) {
-//						System.out.println("둘 중 하나 밖으로 성공 : " + nx1 + " " + ny1 + " / " + nx2 + " " + ny2);
 						return count;
 					}
 					
@@ -118,15 +137,16 @@ public class Main {
 						continue;
 					}
 					
-					// 방문처리??
-					// 두 동전 모두 이동하지 못하고 원래 자리에 그대로 있으면 큐에 담지 않고, 다음 탐색 -> 이동 count만 증가시키고 반복하게 되기 때문
-					if(nx1 == coins[0].x && ny1 == coins[0].y && nx2 == coins[1].x && ny2 == coins[1].y) {
+					// 두 동전 모두 이미 방문한 위치라면 중복 처리 되므로 건너뛰기
+					String visit = "" + nx1 + ny1 + nx2 + ny2;
+					if(visited.contains(visit)) {
 						continue;
 					}
 					
 //					System.out.println(nx1 + " " + ny1 + " / " + nx2 + " " + ny2 + " / count = " + count);
 					
 					queue.add(new Node[] {new Node(nx1, ny1), new Node(nx2, ny2)});
+					visited.add(visit); // 방문처리
 				}
 			}
 			count++;
