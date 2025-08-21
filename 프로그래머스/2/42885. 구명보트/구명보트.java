07f3c7800c5ecdab286310c57ecb7539c2e303d5
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-08-21 16:32:57 +0900

    [level 2] Title: 구명보트, Time: 14.06 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        int answer = 0;
+        while(left <= right) {
+            if(people[left] + people[right] > limit) {
+                answer++;
+                right--;
+            } else {
+                answer++;
+                left++;
+                right--;
+            }
+        }
+        
+        return answer;
+    }
+}
