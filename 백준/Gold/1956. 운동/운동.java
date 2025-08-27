16732e053be50c03bf27d8414c79c784e5869d78
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-08-27 12:22:38 +0900

    [Gold IV] Title: 운동, Time: 576 ms, Memory: 59252 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -10,19 +10,25 @@ import java.util.StringTokenizer;
  * 모든 정점에서 모든 정점까지의 최단경로를 구해 a->b로 가는 최단 경로와 b->a로 가는 최단 경로를 더하는 방식으로 사이클 최단 경로를 구한다.
  * 따라서 일단 플로이드워셜 알고리즘 적용.
  * 
- * 1. 2차원 배열을 INF로 초기화
+ * 1. 2차원 배열을 최댓값(INF)으로 초기화
  * 2. 배열에 a->b로 가는 거리 저장 (단방향 그래프이므로 대칭x)
  * 3. 3중 for문으로 최단 거리 갱신
  * 4. a->b로 가는 최단 거리와 b->a로 가는 최단 거리를 더해 사이클의 최단 거리 구하기
  * 		(주의할 점은 경로를 찾는 것이 불가능할 경우에는 -1 출력. 따라서 사이클 최단 거리가 INF보다 크면 경로가 없다는 것이므로 -1 출력)
+ * 
+ * 주의할 점은 INF의 값을 얼마로 가져가느냐.
+ * 처음에는 가중치의 값이 10,000이므로 20,001로 INF를 설정했는데
+ * 모든 가중치의 값이 10,000이고 정점이 최대 400개 있고, 모든 정점을 돌아 사이클을 만든다면 거리의 합은 최대 400 x 10,000 = 400백만이 된다.
+ * 그러면 사실 거리가 있는건데 최소값을 갱신하는 경우에서 400만보다 작은 20,001이 최소값으로 갱신될 수 있다.
+ * 즉, 실제 거리가 있는데 없는 것으로 잘못 갱신될 수 있다는 것이다.
+ * 따라서 INF의 값을 400만 보다 훨씬 큰 숫자로 설정해야 한다.
+ * 아니면 안정적으로 그냥 Integer.MAX_VALUE로 가져가는 것이 좋을 수 있다.
  * 
  * @author YooSejin
  *
  */
 
 public class Main {
-	
-//	static final int INF = 20001;
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,7 +41,6 @@ public class Main {
 		
         // 1. 배열 초기화
         for(int i=0; i<=V; i++) {
-//        	Arrays.fill(arr[i], INF); // a->b로 가는 거리가 존재하지 않을 때 INF값을 가짐. 그리고 i부터 k까지의 거리와 k부터 j까지의 거리의 합보다 무조건 큰 값이어야 함. 그래야 최대로 거리가 존재할 때에도 최소값으로 arr[i][j]를 저장할 수 있음.
         	Arrays.fill(arr[i], Integer.MAX_VALUE);
         	arr[i][i] = 0;
         }
@@ -63,14 +68,13 @@ public class Main {
         for(int i=1; i<=V; i++) {
         	for(int j=1; j<=V; j++) {
         		if(i == j) continue;
-//        		if(arr[i][j] != INF && arr[j][i] != INF) { // a와 b 사이에 둘 다 경로가 있어야만 사이클 가능
+        		// a와 b 사이에 둘 다 경로가 있어야만 사이클 가능
         		if(arr[i][j] != Integer.MAX_VALUE && arr[j][i] != Integer.MAX_VALUE) {
         			answer = Math.min(answer, arr[i][j] + arr[j][i]);
         		}
         	}
         }
         
-//        System.out.println(answer == INF ? -1 : answer);
         System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
 	}
 
