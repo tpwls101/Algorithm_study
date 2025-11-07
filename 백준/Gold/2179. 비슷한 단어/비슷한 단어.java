7b793070a47dd0e600b370cd7d54011febb10fe6
유세진 <97137124+tpwls101@users.noreply.github.com>
2025-11-07 15:10:07 +0900

    [Gold IV] Title: 비슷한 단어, Time: 200 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -4,6 +4,12 @@ import java.io.InputStreamReader;
 
 /**
  * <BJ_2179_비슷한단어>
+ * 처음에는 입력 순서대로 단어에 인덱스를 부여해 map에 저장하고 단어는 정렬를 해서 비교했는데
+ * 그러면 공통 접두사의 길이가 같은 경우에 문제가 생겼다.
+ * 예를 들어 aaaa, aaab, aabd, aabc가 있으면
+ * aaa의 공통 접두사의 길이가 3이라 두 단어의 인덱스 번호를 저장하고 이후 aab의 공통 접두사의 길이도 3이라 두 단어를 저장하게 되는 것이다.
+ * 이런 상황 등을 고려하고 입력 순서를 유지하기 위해서는 그냥 주어진 순서대로 비교하는 것이 가장 안전하다.
+ * 따라서 i번째 단어와 j번째 단어(j=i+1부터 N까지)를 비교하며 공통 접두사의 길이가 가장 크다면 가장 입력이 빠른 단어이므로 두 단어를 저장해 출력하면 된다.
  * 
  * @author YooSejin
  *
@@ -28,9 +34,11 @@ public class Main {
         
         for(int i=0; i<N; i++) {
         	String now = arr[i];
+        	if(now.length() <= max) continue; // 시간 최적화 : 가지치기 -> 어차피 최대 공통 접두사의 길이보다 단어의 길이가 짧거나 같으면 새로 갱신하지 못한다.
         	
         	for(int j=i+1; j<N; j++) {
         		String next = arr[j];
+        		if(next.length() <= max) continue; // 시간 최적화 : 가지치기 -> 어차피 최대 공통 접두사의 길이보다 단어의 길이가 짧거나 같으면 새로 갱신하지 못한다.
         		int len = getPrefixLength(now, next); // 공통 접두사의 길이 구하기
         		
         		if(max < len) {
