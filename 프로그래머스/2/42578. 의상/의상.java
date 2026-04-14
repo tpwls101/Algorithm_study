641fc87ddb904b7ada4ee8eb6af32d74d47c8c0a
유세진 <97137124+tpwls101@users.noreply.github.com>
2026-04-15 01:06:03 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
-/**
-한 종류에 대해 안입는 경우도 포함되므로 의상개수+1 로 계산
-다만, 모두 안입는 경우는 포함하면 안되므로 최종 결과에서 -1
+/*
+1. 종류별로 가능한 옷의 개수를 map에 저장
+2. 옷을 선택 안하는 것도 하나의 방법이므로 각 종류별로 +1 한 후 곱해 경우의 수 계산
+3. 단, 모두 선택 안하는 공집합의 경우는 불가하므로 -1
 */
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        
+        for(String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
+        
+        int answer = 1;
         
         for(int value : map.values()) {
             answer *= (value + 1);
         }
         
-        return answer - 1; // 아무것도 안입는 경우의 수 제외
+        return answer - 1;
     }
 }
