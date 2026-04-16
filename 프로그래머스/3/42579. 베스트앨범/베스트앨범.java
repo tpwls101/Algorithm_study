f1e3f2e086de38e77d55d1c4bd18a2781e3ed614
유세진 <97137124+tpwls101@users.noreply.github.com>
2026-04-16 23:57:25 +0900

    [level 3] Title: 베스트앨범, Time: 4.48 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    
+    public class TotalPlay {
+        String genre;
+        int totalPlay;
+        
+        TotalPlay(String genre, int totalPlay) {
+            this.genre = genre;
+            this.totalPlay = totalPlay;
+        }
+    }
+    
+    public class Song {
+        int index;
+        int play;
+        
+        Song(int index, int play) {
+            this.index = index;
+            this.play = play;
+        }
+    }
+    
+    public List solution(String[] genres, int[] plays) {
+        Map<String, Integer> map = new HashMap<>(); // 장르별 총 재생 횟수
+        
+        for(int i=0; i<genres.length; i++) {
+            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+        }
+        
+        List<TotalPlay> list = new ArrayList<>();
+        
+        for(String genre : map.keySet()) {
+            list.add(new TotalPlay(genre, map.get(genre)));
+        }
+        
+        Collections.sort(list, (o1, o2) -> o2.totalPlay - o1.totalPlay);
+        
+        List<Integer> answerList = new ArrayList<>();
+        
+        for(TotalPlay tp : list) {
+            //System.out.println(tp.totalPlay);
+            List<Song> songList = new ArrayList<>(); // 장르별 노래의 재생 횟수 저장
+            
+            for(int i=0; i<genres.length; i++) {
+                if(genres[i].equals(tp.genre)) {
+                    songList.add(new Song(i, plays[i]));
+                }
+            }
+            
+            Collections.sort(songList, (o1, o2) -> o2.play - o1.play);
+            
+            if(songList.size() < 2) {
+                answerList.add(songList.get(0).index);
+            } else {
+                for(int i=0; i<2; i++) {
+                    answerList.add(songList.get(i).index);
+                }
+            }
+        }
+        
+        return answerList;
+    }
+}
